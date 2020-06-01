089523688f10501cd3e24ed454c124125a80416c
Ryan McCarthy <ryanmccarthypdx@gmail.com>
2020-06-01 11:17:39 -0700

    Configure config

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,6 +1,6 @@
 Config.setup do |config|
   # Name of the constant exposing loaded settings
-  config.const_name = 'Settings'
+  config.const_name = 'OmnibotConfig'
 
   # Ability to remove elements of the array set in earlier loaded settings file. For example value: '--'.
   #
@@ -17,7 +17,7 @@ Config.setup do |config|
 
   # Load environment variables from the `ENV` object and override any settings defined in files.
   #
-  # config.use_env = false
+  config.use_env = true
 
   # Define ENV variable prefix deciding which variables to load into config.
   #
@@ -26,23 +26,24 @@ Config.setup do |config|
   #
   # When not set it defaults to `config.const_name`.
   #
-  config.env_prefix = 'SETTINGS'
+  config.env_prefix = 'OMNIBOTCONFIG'
 
   # What string to use as level separator for settings loaded from ENV variables. Default value of '.' works well
   # with Heroku, but you might want to change it for example for '__' to easy override settings from command line, where
   # using dots in variable names might not be allowed (eg. Bash).
   #
   # config.env_separator = '.'
+  config.env_separator = '__'
 
   # Ability to process variables names:
   #   * nil  - no change
   #   * :downcase - convert to lower case
   #
-  # config.env_converter = :downcase
+  config.env_converter = :downcase
 
   # Parse numeric values as integers instead of strings.
   #
-  # config.env_parse_values = true
+  config.env_parse_values = true
 
   # Validate presence and type of specific config values. Check https://github.com/dry-rb/dry-validation for details.
   #
